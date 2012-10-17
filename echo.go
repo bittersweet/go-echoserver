3384af5897a8d9140670ee4531a4542d62236515
Mark Mulder <markmulder@gmail.com>
2012-10-17 21:35:24 +0200

    Format go code

--- echo.go
+++ echo.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
-    "go-echoserver/src/metric"
+	"fmt"
+	"go-echoserver/src/metric"
+	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path[1:] != "favicon.ico" {
-      go metric.Track("view")
-    }
-    fmt.Printf("[%s] ", r.RemoteAddr)
-    fmt.Printf("%s ", r.Method)
-    fmt.Printf("%s\n", r.URL.Path)
+	if r.URL.Path[1:] != "favicon.ico" {
+		go metric.Track("view")
+	}
+	fmt.Printf("[%s] ", r.RemoteAddr)
+	fmt.Printf("%s ", r.Method)
+	fmt.Printf("%s\n", r.URL.Path)
 
-    fmt.Fprintf(w, "%s\n", r.URL.Path[1:])
+	fmt.Fprintf(w, "%s\n", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    port := os.Getenv("PORT")
-    fmt.Println("listening on port " + port)
-    http.ListenAndServe(":"+port, nil)
+	http.HandleFunc("/", handler)
+	port := os.Getenv("PORT")
+	fmt.Println("listening on port " + port)
+	http.ListenAndServe(":"+port, nil)
 }
