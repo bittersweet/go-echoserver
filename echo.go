e957621f0722d990d54163550cf60cc1ab935982
Mark Mulder <markmulder@gmail.com>
2012-10-17 21:27:09 +0200

    Remove own timestamp as Heroku provides this

--- echo.go
+++ echo.go
@@ -3,13 +3,10 @@ package main
 import (
     "fmt"
     "net/http"
-    "time"
     "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    t := time.Now()
-    fmt.Printf("%s ", t)
     fmt.Printf("[%s] ", r.RemoteAddr)
     fmt.Printf("%s ", r.Method)
     fmt.Printf("%s\n", r.URL.Path)
